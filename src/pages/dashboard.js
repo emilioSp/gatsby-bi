c4d77a34b24b6e95c8b136b06bc00e0381a8657e
emilioSp <emilio.spatola@gmail.com>
2021-02-11 18:31:30 +0100

    Add Layout

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Chart from '../components/Chart.js';
+import { Layout } from '../components/Layout.js';
 
 const Dashboard = () => {
   return (
-    <div className="container">
-      <h1>Esempio di chart</h1>
-      <Chart />
-    </div>
+    <Layout>
+      <div className="container">
+        <h1>Esempio di chart</h1>
+        <Chart />
+      </div>
+    </Layout>
   )
 };
 
